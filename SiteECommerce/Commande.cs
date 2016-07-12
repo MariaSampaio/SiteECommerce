e1a23cf218f387c153bb39f2fb3cd85f66868884
valentin <valentin@POSTE_5>
2016-07-12 16:27:04 +0200

    Suite des Class métier

---
+++ SiteECommerce/Commande.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiteECommerce.Metier
+{
+    public class Commande
+    {
+        [Key]
+        public int Id { get; set; }
+        public Client Clients { get; set; }
+        public ICollection<Produit> Produits { get; set; }
+        public int Quantite { get; set; }
+        public decimal prixTotal { get; set; }
+    }
+}
