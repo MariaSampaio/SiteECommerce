3f95a11263eb33d6ec77b72af7d2ca7f19380104
Waelle <Waelle.gouja@gmail.com>
2016-07-13 10:11:04 +0200

    modification_configuration

--- SiteECommerce.Mvc/Migrations/Configuration.cs
+++ SiteECommerce.Mvc/Migrations/Configuration.cs
@@ -32,16 +32,18 @@ namespace SiteECommerce.Mvc.Migrations
             var colgate = new Metier.Marque { NomMarque = "Colgate" };
             context.Marques.AddOrUpdate(m => m.NomMarque, dop, oreal, colgate);
 
-            context.Produits.AddOrUpdate( p => p.NomProduit,
-            new Metier.Produit { NomProduit = "Savon", ImgProduit = "img", PrixProduit = 15, DescriptionProduit = "naturel", Marque = dop, CategorieId =1 },
-            new Metier.Produit { NomProduit = "Shampoing", ImgProduit = "img", PrixProduit = 12, DescriptionProduit = "chimique", Marque = dop, CategorieId = 1 },
-            new Metier.Produit { NomProduit = "Gel Douche", ImgProduit = "img", PrixProduit = 16, DescriptionProduit = "chimique", Marque = oreal, CategorieId = 1 });
-
-
             var cosmetique = new Metier.Categorie { NomCategorie = "Cosmetique" };
             var hygiene = new Metier.Categorie { NomCategorie = "Hygiene" };
             var soin = new Metier.Categorie { NomCategorie = "Soin" };
             context.Categories.AddOrUpdate(c => c.NomCategorie, cosmetique, hygiene, soin);
+
+            context.Produits.AddOrUpdate( p => p.NomProduit,
+            new Metier.Produit { NomProduit = "Savon", ImgProduit = "img", PrixProduit = 15, DescriptionProduit = "naturel", Marque = dop, Categorie =cosmetique },
+            new Metier.Produit { NomProduit = "Shampoing", ImgProduit = "img", PrixProduit = 12, DescriptionProduit = "chimique", Marque = dop, Categorie = cosmetique },
+            new Metier.Produit { NomProduit = "Gel Douche", ImgProduit = "img", PrixProduit = 16, DescriptionProduit = "chimique", Marque = oreal, Categorie = cosmetique });
+
+
+           
             
 
         }
